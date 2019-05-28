ee05912510570c4f15035c6d3c780f699887ff28
Enrique Matos Alfonso <gardero@gmail.com>
2019-05-28 19:27:13 +0300

    more implementation

--- bridge.py
+++ bridge.py
@@ -21,25 +21,32 @@ def results():
 
     if action == "glucose-level":
         url = "%s/api/v1/entries" % os.environ.get('NIGHTSCOUT_ADDRESS')
-
         querystring = {"count": "1"}
+        response = call_api(querystring, url)
+        # return a fulfillment response
+        return {'fulfillmentText': 'Your glucose level is %d' % json.loads(response.text)[0]["sgv"]}
+    if action == "carbs-insulin-intake":
+        # url = "%s/api/v1/entries" % os.environ.get('NIGHTSCOUT_ADDRESS')
+        # querystring = {"count": "1"}
+        # response = call_api(querystring, url, "POST")
+        # # return a fulfillment response
+        return {'fulfillmentText': 'Sorry, the functionality is not completely implemented'}
+    else:
+        return {'fulfillmentText': 'Sorry, I cannot provide that information'}
 
-        headers = {
-            'Accept': "application/json",
-            'Acces': "",
-            'cache-control': "no-cache",
-            'Postman-Token': "8602c936-466f-41b0-9a45-e23099216bcb"
-        }
 
-        print("calling: " + url)
-
-    response = requests.request("GET", url, headers=headers, params=querystring)
-
+def call_api(querystring, url, method="GET"):
+    headers = {
+        'Accept': "application/json",
+        'Acces': "",
+        'cache-control': "no-cache",
+        'Postman-Token': "8602c936-466f-41b0-9a45-e23099216bcb"
+    }
+    print("calling: " + url)
+    response = requests.request(method, url, headers=headers, params=querystring)
     print(response.text)
+    return response
 
-
-    # return a fulfillment response
-    return {'fulfillmentText': 'Your glucose level is %d' % json.loads(response.text)[0]["sgv"]}
 
 # create a route for webhook
 @app.route('/webhook', methods=['GET', 'POST'])
